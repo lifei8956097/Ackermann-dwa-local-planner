6b67616c854cd9c0141c347fe293fe7de7a219fb
lifei <lifei@aimo.ai>
2021-08-03 18:17:18 +0800

    using google style

--- src/dwa_planner_node.cpp
+++ src/dwa_planner_node.cpp
@@ -1,9 +1,8 @@
 #include "dwa_planner/dwa_planner.h"
 
-int main(int argc, char** argv)
-{
-    ros::init(argc, argv, "dwa_planner");
-    DWAPlanner planner;
-    planner.process();
-    return 0;
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "dwa_planner");
+  DWAPlanner planner;
+  planner.process();
+  return 0;
 }
--- test/dwa_planner_test.cpp
+++ test/dwa_planner_test.cpp
@@ -4,34 +4,32 @@
 
 #include "dwa_planner/dwa_planner.h"
 
-TEST(TestSuite, test1)
-{
-    std::cout << "test calc dynamic window" << std::endl;
-    DWAPlanner dwa;
-    geometry_msgs::Twist vel;
-    vel.linear.x = 0.0;
-    vel.angular.z = 0.0;
-    DWAPlanner::Window window = dwa.calc_dynamic_window(vel);
-    EXPECT_LE(window.min_velocity, window.max_velocity);
-    EXPECT_LE(window.min_omega, window.max_omega);
+TEST(TestSuite, test1) {
+  std::cout << "test calc dynamic window" << std::endl;
+  DWAPlanner dwa;
+  geometry_msgs::Twist vel;
+  vel.linear.x = 0.0;
+  vel.angular.z = 0.0;
+  DWAPlanner::Window window = dwa.calc_dynamic_window(vel);
+  EXPECT_LE(window.min_velocity, window.max_velocity);
+  EXPECT_LE(window.min_omega, window.max_omega);
 }
 
-int main(int argc, char** argv)
-{
-    testing::InitGoogleTest(&argc, argv);
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
 
-    ros::init(argc, argv, "dwa_planner_test");
+  ros::init(argc, argv, "dwa_planner_test");
 
-    ros::AsyncSpinner spinner(1);
-    spinner.start();
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
 
-    ros::Duration(3.0).sleep();
+  ros::Duration(3.0).sleep();
 
-    int r_e_t = RUN_ALL_TESTS();
+  int r_e_t = RUN_ALL_TESTS();
 
-    spinner.stop();
+  spinner.stop();
 
-    ros::shutdown();
+  ros::shutdown();
 
-    return r_e_t;
+  return r_e_t;
 }
